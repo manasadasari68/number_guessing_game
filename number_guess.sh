5ee1c1645e9b284a9f13320186413c89f0046376
manasadasari68 <118374185+manasadasari68@users.noreply.github.com>
2026-03-11 17:45:35 +0530

    feat: implement number guessing game loop
    Add number guessing game logic with input validation.

--- number_guess.sh
+++ number_guess.sh
@@ -18,3 +18,32 @@ else
 
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+echo "Guess the secret number between 1 and 1000:"
+
+NUMBER_OF_GUESSES=0
+
+while true
+do
+  read GUESS
+
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  ((NUMBER_OF_GUESSES++))
+
+  if [[ $GUESS -gt $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that, guess again:"
+  elif [[ $GUESS -lt $SECRET_NUMBER ]]
+  then
+    echo "It's higher than that, guess again:"
+  else
+    break
+  fi
+done
